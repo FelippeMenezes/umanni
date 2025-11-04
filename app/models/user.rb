2a5e3f821fc2e76068c0b0fbcc26eb370373be8d
Felippe Menezes <felippemenezes1982@gmail.com>
2025-11-04 18:21:43 -0300

    Active Storage install to upload and associate image touser model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,28 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  has_one_attached :avatar
+
+  # Atributo virtual para receber a URL da imagem
+  attr_accessor :avatar_url
+
+  before_validation :attach_avatar_from_url
+
+  private
+
+  def attach_avatar_from_url
+    return if avatar_url.blank?
+    return if avatar.attached?
+
+    require 'open-uri'
+
+    begin
+      downloaded_image = URI.open(avatar_url)
+      self.avatar.attach(io: downloaded_image, filename: "avatar.jpg")
+    rescue OpenURI::HTTPError, Errno::ENOENT => e
+      errors.add(:avatar_url, "não é uma URL de imagem válida")
+      Rails.logger.error("Erro ao baixar avatar da URL: #{avatar_url}. Erro: #{e.message}")
+    end
+  end
 end
