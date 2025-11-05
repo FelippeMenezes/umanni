4214fff75668a9a0cc993dc22064d5b651d186ab
Felippe Menezes <felippemenezes1982@gmail.com>
2025-11-05 15:33:37 -0300

    Create user for user admin

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -6,11 +6,36 @@ class Admin::UsersController < ApplicationController
     @users = User.all.order(created_at: :desc)
   end
 
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    
+    if @user.save
+      redirect_to admin_users_path, notice: "Usuário criado com sucesso!"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
   private
 
   def require_admin
     unless current_user.admin?
       redirect_to root_path, alert: "Acesso negado. Apenas administradores podem acessar esta área."
     end
+  end
+
+  def user_params
+    # Process role parameter from toggle first
+    if params[:user][:role] == "admin" || params[:user][:role] == true || params[:user][:role] == "1"
+      params[:user][:role] = "admin"
+    else
+      params[:user][:role] = "user"
+    end
+    
+    params.require(:user).permit(:email, :full_name, :role, :password, :password_confirmation)
   end
 end
