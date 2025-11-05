a1bea88f3b49222b4126b2f837e1fae36b226733
Felippe Menezes <felippemenezes1982@gmail.com>
2025-11-05 14:30:56 -0300

    User list for admin role

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_11_04_210730) do
+ActiveRecord::Schema[7.1].define(version: 2025_11_05_170013) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -52,6 +52,11 @@ ActiveRecord::Schema[7.1].define(version: 2025_11_04_210730) do
     t.datetime "updated_at", null: false
     t.string "full_name"
     t.integer "role"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
